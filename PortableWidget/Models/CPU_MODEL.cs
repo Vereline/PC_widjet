b48a04265ab6ea7eacadbbdb4ddccf8cb7b028b6
stepan <stepansavel97@gmail.com>
2017-11-16 16:15:35 +0100

    models created, i hope this is what we need.

---
+++ PortableWidget/Models/CPU_MODEL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableWidget.Models
+{
+    class CPU_MODEL
+    {
+        float In_use { get; set; } // usage of CPU
+        int Temperature { get; set; } //current CPU temperature
+        float Speed { get; set; } //current core speed in GHz
+        int Processes { get; set; } //current amount of processes
+        int Threads { get; set; } //current amount of threads
+    }
+}
---
+++ PortableWidget/Models/DISK_MODEL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableWidget.Models
+{
+    class DISK_MODEL
+    {
+        float Read_speed { get; set; } //current read speed
+        float Write_speed { get; set; } //current write speed
+        float Average_response_time { get; set; } //average response time in ms
+        int Capacity { get; set; } //current disk capacity
+        int Formatted { get; set; } //foramatted disk space
+    }
+}
