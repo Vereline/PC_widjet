4b2cb53497f559a80503e464f8cacadecd4ad360
stepan <stepansavel97@gmail.com>
2017-12-05 21:40:32 +0300

    Analizer

---
+++ PortableWidget/Core/Analizer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using PortableWidget.Models;
+using PortableWidget.Data;
+
+namespace PortableWidget.Core
+{
+    public class Analizer
+    {
+        bool isRun = true;
+        int timeout = 1000;
+        Thread cpuAnalizeThread;
+        Thread ramAnalizeThread;
+
+        public Analizer(int timeout) {
+            this.timeout = timeout;
+        }
+
+        public void Start()
+        {
+            isRun = true;
+            cpuAnalizeThread = new Thread(AnalizeCpu);
+            ramAnalizeThread = new Thread(AnalizeRam);
+            cpuAnalizeThread.Start();
+            ramAnalizeThread.Start();
+            
+        }
+
+        public void Stop() {
+            isRun = false;
+        }
+
+        private void AnalizeCpu() {
+            Cpu cpu = new Core.Cpu();
+            while (isRun) {
+                lock (CpuData.cpuData) {
+                    CpuData.cpuData.Add(new CpuModel() {
+                        UsagePercentage = cpu.GetUsagePercentage()
+                        // TODO - all props
+                    });
+                }
+                Thread.Sleep(timeout);
+            }
+        }
+
+        private void AnalizeRam()
+        {
+        }
+    }
+}
---
+++ PortableWidget/Data/CpuData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PortableWidget.Models;
+
+namespace PortableWidget.Data
+{
+    public static class CpuData
+    {
+        public static List<CpuModel> cpuData { get; set; } = new List<CpuModel>();
+    }
+}
