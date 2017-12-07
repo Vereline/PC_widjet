1840a68e6c4202d8da6e90f8c7b8da2f2dcc289a
Vereline <vstanko1998@gmail.com>
2017-12-07 03:09:37 +0300

    Added cpu chart
    Has a bug in library(!), which freezes charts(help, but not fix is to change tabs as fast as user can )

--- PortableWidget/Pages/CpuPage.xaml.cs
+++ PortableWidget/Pages/CpuPage.xaml.cs
@@ -16,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Configurations;
 using PortableWidget.Data;
 
 namespace PortableWidget.Pages
@@ -25,6 +27,12 @@ namespace PortableWidget.Pages
     /// </summary>
     public partial class CpuPage : Page
     {
+        public class MeasureModel
+        {
+            public DateTime DateTime { get; set; }
+            public double Value { get; set; }
+        }
+
         public class CpuDataClass: INotifyPropertyChanged
         {
             private string _id;
@@ -36,14 +44,66 @@ namespace PortableWidget.Pages
             int timeout = 1000;
             public Thread getDataThread;
 
-            private void StopThread()
+            private double _axisMax;
+            private double _axisMin;
+            private double _trend;
+
+            public ChartValues<MeasureModel> ChartValues { get; set; }
+            public Func<double, string> DateTimeFormatter { get; set; }
+            public double AxisStep { get; set; }
+            public double AxisUnit { get; set; }
+
+            public double AxisMax
+            {
+                get { return _axisMax; }
+                set
+                {
+                    _axisMax = value;
+                    OnPropertyChanged("AxisMax");
+                }
+            }
+            public double AxisMin
+            {
+                get { return _axisMin; }
+                set
+                {
+                    _axisMin = value;
+                    OnPropertyChanged("AxisMin");
+                }
+            }
+
+            private void SetAxisLimits(DateTime now)
+            {
+                AxisMax = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 1 second ahead
+                AxisMin = now.Ticks - TimeSpan.FromSeconds(8).Ticks; // and 8 seconds behind
+            }
+
+            public void StopThread()
             {
                 isRunning = false;
             }
-
+            public void StartThread()
+            {
+                isRunning = true;
+            }
 
             public CpuDataClass(int i)
             {
+                var mapper = Mappers.Xy<MeasureModel>()
+                .X(model => model.DateTime.Ticks)
+                .Y(model => model.Value);
+
+                Charting.For<MeasureModel>(mapper);
+
+                ChartValues = new ChartValues<MeasureModel>();
+
+                DateTimeFormatter = value => new DateTime((long)value).ToString("hh:mm:ss");
+
+                AxisStep = TimeSpan.FromSeconds(1).Ticks;
+                AxisUnit = TimeSpan.TicksPerSecond;
+
+                SetAxisLimits(DateTime.Now);
+
                 if (i >= 0)
                 {
                     return;
@@ -66,12 +126,25 @@ namespace PortableWidget.Pages
                     {
                         RefreshBinding();
                     }
+                    var now = DateTime.Now;
+                    _trend = _usagePercentage;
+
+                    ChartValues.Add(new MeasureModel
+                    {
+                        DateTime = now,
+                        Value = _trend
+                    });
+
+                    SetAxisLimits(now);
+                    System.Console.Write("now {0}", now);
+                    System.Console.Write("value {0}", _trend);
+                    //lets only use the last 150 values
+                    if (ChartValues.Count > 10) ChartValues.RemoveAt(0);
                     Thread.Sleep(timeout);
                 }
                 
             }
 
-            
             public string Id
             {
                 get { return _id; }
@@ -160,6 +233,11 @@ namespace PortableWidget.Pages
 
         private void CartesianChart_Loaded(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //_cpuDataClass.StopThread();
         }
     }
 }
